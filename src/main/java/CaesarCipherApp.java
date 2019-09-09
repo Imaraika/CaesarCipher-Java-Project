b944ba379bda969ccf2bb96053311e335d2764fb
Imaraika <inange2013@gmail.com>
2019-09-09 16:31:23 +0200

    changes have been added

--- src/main/java/CaesarCipherApp.java
+++ src/main/java/CaesarCipherApp.java
@@ -9,12 +9,12 @@ public class CaesarCipherApp {
         String plaintext = sc.nextLine();
         System.out.println(" Enter the value by which each character in the plaintext message gets shifted : ");
         int shift = sc.nextInt();;
-        Encryption encrypty = new Encryption();
-        String pingPongResult = encrypty.encrypt(plaintext, 3);
+//        Encryption encrypty = new Encryption();
+//        String pingPongResult = encrypty.encrypt(plaintext, 3);
         Decryption decrypt = new Decryption();
         String pingPongResult2 = decrypt.decrypt(plaintext, 3);
 
-        System.out.println(pingPongResult);
+//        System.out.println(pingPongResult);
         System.out.println(pingPongResult2);
         sc.close();
     }
